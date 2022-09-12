a6a578c1ddbad39dcbab91f49f93c756801ce922
Serondrych <serondrych@gmail.com>
2022-09-11 19:03:43 -0700

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ = '__main__':
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments".format(num - 1)
+    elif num == 2:
+        print("{} argument:".format(num - 1)
+    else:
+        print("{} arguments:".format(num - 1)
+
+    for n in range(1, number):
+        print("{}: {}".format(1, sys.argv([n]))
