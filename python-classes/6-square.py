36e2e31b2c3400a51f2361609983c431cb0d106a
Serondrych <serondrych@gmail.com>
2022-09-25 18:59:12 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -49,11 +49,12 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for n in range(0, self.__positin[1]):
-            print()
-        for i in range(0, self.__size):
-            for var in range(0, self.__position[0]):
-                print(" ", end="")
+        else: 
+            for i in range(0, self.__positin[1]):
+                print()
             for j in range(0, self.__size):
-                print("#", end="")
-            print()
+                for h in range(0, self.__position[0]):
+                    print(" ", end="")
+                for i in range(0, self.__size):
+                    print("#", end="")
+                print()
