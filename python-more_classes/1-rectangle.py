ef09204482b2d7c28146d467c03d399e819366ad
Serondrych <serondrych@gmail.com>
2022-10-07 13:35:07 -0300

    1-square

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define Rectangle as a class"""
+
+
+class Rectangle:
+    """Width"""
+    def __init__(self, value):
+        """initialize rectangle"""
+        self.__width = width
+
+    @property:
